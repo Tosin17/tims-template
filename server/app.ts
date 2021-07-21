17c53e9950fc5aa4e051af6176064fb7492cdeaf
Tosin17 <kajopelayeola@gmail.com>
2021-07-21 00:13:58 -0400

    Clean ups

--- server/app.ts
+++ server/app.ts
@@ -1,14 +1,50 @@
-const express = require('express')
+import express from 'express'
 const server = express()
 const port = 5000
 
-import { getCustomers } from './src/db/queries'
+import {
+    getCustomers,
+    getCustomersHistory,
+    getActiveCustomersStatus,
+    getActiveCustomersTypes,
+    getCustomersStatus,
+    getCustomersTypes,
+} from './src/db/queries'
 
-server.get('/', (req: any, res: any) => {
-    getCustomers().then(({ recordset }) => {
-        res.json(recordset)
-    })
+server.get('/', async (req: express.Request, res: express.Response) => {
+    const { recordset } = await getCustomers()
+    res.json(recordset)
 })
+
+server.get(
+    '/customer-history',
+    async (req: express.Request, res: express.Response) => {
+        const { recordset } = await getCustomersHistory()
+        res.json(recordset)
+    }
+)
+
+server.get(
+    '/customer-status',
+    async (req: express.Request, res: express.Response) => {
+        const status = req.query.active
+        const { recordset } = status
+            ? await getActiveCustomersStatus()
+            : await getCustomersStatus()
+        res.json(recordset)
+    }
+)
+
+server.get(
+    '/customer-types',
+    async (req: express.Request, res: express.Response) => {
+        const status = req.query.active
+        const { recordset } = status
+            ? await getActiveCustomersTypes()
+            : await getCustomersTypes()
+        res.json(recordset)
+    }
+)
 
 server.listen(port, () => {
     console.log(`Listening on ${port}`)
--- server/src/db/queries.ts
+++ server/src/db/queries.ts
@@ -9,3 +9,74 @@ export async function getCustomers() {
         throw e
     }
 }
+
+export async function getCustomersHistory() {
+    try {
+        const sql = await connectToDB()
+        const result = await sql.query`select * from tblCustomerHistory`
+        return result
+    } catch (e) {
+        throw e
+    }
+}
+
+export async function getActiveCustomersStatus() {
+    try {
+        const sql = await connectToDB()
+        const result =
+            await sql.query`SELECT CustomerStatusID, Name as CustomerStatus
+        FROM Timothy.dbo.tblCustomerStatus
+        WHERE Active <> 0`
+        return result
+    } catch (e) {
+        throw e
+    }
+}
+
+export async function getCustomersStatus() {
+    try {
+        const sql = await connectToDB()
+        const result =
+            await sql.query`SELECT * FROM Timothy.dbo.tblCustomerStatus`
+        return result
+    } catch (e) {
+        throw e
+    }
+}
+
+export async function getActiveCustomersTypes() {
+    try {
+        const sql = await connectToDB()
+        const result =
+            await sql.query`SELECT CustomerTypeID, Name as CustomerType
+            FROM Timothy.dbo.tblCustomerType
+            WHERE Active <> 0
+            `
+        return result
+    } catch (e) {
+        throw e
+    }
+}
+
+export async function getCustomersTypes() {
+    try {
+        const sql = await connectToDB()
+        const result =
+            await sql.query`SELECT * FROM Timothy.dbo.tblCustomerType`
+        return result
+    } catch (e) {
+        throw e
+    }
+}
+
+export async function addCustomer(details: unknown) {
+    try {
+        const sql = await connectToDB()
+        const result = await sql.query`INSERT INTO [Timothy].[dbo].[tblCustomer]
+            (Name, Active, Date, CustomerStatusID, CustomerTypeID, Note, Archived, UserID, DateAdded, DateUpdated)
+            VALUES ('abc', 1, getdate(), 1, 2, 'Note', 0, 'Timothy', '2020-01-02 13:18:13.520', '2020-01-03 14:19:14.521')`
+        return result
+    } catch (e) {
+        throw e
+    }
+}
